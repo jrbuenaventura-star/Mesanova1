1d47f5a7af987574c4e75b472c8f42eadff9f11c
jrbuenaventura-star <jrbuenaventura@gmail.com>
2026-01-21 06:08:15 -0500

    v43

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type React from "react"
 import type { Metadata } from "next"
+import Script from "next/script"
 
 import { Analytics } from "@vercel/analytics/next"
 import { SiteHeader } from "@/components/site-header"
@@ -44,7 +45,32 @@ export default function RootLayout({
 
   return (
     <html lang="es">
+      <head>
+        {/* Google Tag Manager */}
+        <Script
+          id="gtm-script"
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WNCMDR6D');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
+      </head>
       <body className={`font-sans antialiased`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WNCMDR6D"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         {gaId && <GoogleAnalytics gaId={gaId} />}
         {pixelId && <MetaPixel pixelId={pixelId} />}
         <CrossDomainLinker />
